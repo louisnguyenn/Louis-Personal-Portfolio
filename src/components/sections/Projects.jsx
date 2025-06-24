93a5919045f86355d7746034e46ff35fdf6cec37
louis nguyen <louis.nguyen550@gmail.com>
2025-06-23 23:23:14 -0400

    project update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -58,6 +58,29 @@ export const Projects = () => {
 
           {/* project 3 */}
           <div className="bg-[#05091e] relative p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#AA8F76] hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all duration-300">
+            <h3 className="text-xl font-bold mb-2">Air Quality Analysis in Cities</h3>
+
+            <div className="flex flex-wrap grap-2 mb-4">
+              {["Python", "Pandas", "Matplotlib", "Seaborn", "NumPy", "Jupyter Notebook"].map((tech, key) => (
+                <span key={key} className="bg-[#AA8F76]/10 text-[#AA8F76] border border-[#AA8F76]/30 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition duration-300">
+                  {tech}
+                </span>
+              ))}
+            </div>
+
+            <p className="text-gray-400 mt-4 mb-4">
+              This project focuses on examining air quality metrics across different cities, using Python to uncover patterns
+              in environmental data and assess urban air pollution levels, Pandas and NumPy for data manipulation, Matplotlib and Seaborn 
+              for data visualizations, and Jupyter Notebook for a nice and clean summary of key findings.
+            </p>
+
+            <div className="flex justify-between items-center ">
+              <a href="https://github.com/louisnguyenn/Global_Air_Quality_Data_Analysis" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)]">View Project</a>
+            </div>
+          </div>
+
+          {/* project 4 */}
+          <div className="bg-[#05091e] relative p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#AA8F76] hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all duration-300">
             <h3 className="text-xl font-bold mb-2">Job Vacancies in Canada Data Analysis</h3>
 
             <div className="flex flex-wrap grap-2 mb-4">
@@ -69,41 +92,18 @@ export const Projects = () => {
             </div>
 
             <p className="text-gray-400 mt-4 mb-4">
-              This project focuses on analyzing job vacancy trends across Canada using large CSV datasets.
-              Using Python's powerful data manipulation and visualization libraries, the goal is to uncover meaningful
-              insights into employment demand across various sectors and provinces.
+              This project focuses on analyzing job vacancy trends across Canada using large CSV datasets. 
+              Using Python's powerful data manipulation and visualization libraries, 
+              the goal is to uncover meaningful insights into employment demand across various sectors and provinces.
             </p>
 
             <div className="flex justify-between items-center ">
-              <a href="https://github.com/louisnguyenn/Job-Vacancies-in-Canada-Data-Analysis" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)]">View Project</a>
-            </div>
-          </div>
-
-          {/* project 4 */}
-          <div className="bg-[#05091e] relative p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#AA8F76] hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all duration-300">
-            <h3 className="text-xl font-bold mb-2">Retail Data Analysis</h3>
-
-            <div className="flex flex-wrap grap-2 mb-4">
-              {["SQL", "PostgreSQL", "Microsoft Excel"].map((tech, key) => (
-                <span key={key} className="bg-[#AA8F76]/10 text-[#AA8F76] border border-[#AA8F76]/30 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition duration-300">
-                  {tech}
-                </span>
-              ))}
-            </div>
-
-            <p className="text-gray-400 mt-4 mb-4">
-              This project is designed to demonstrate SQL skills and techniques typically used by data analysts to explore,
-              clean, and analyze retail sales data. The project involves setting up a retail sales database, performing
-              exploratory data analysis (EDA), and answering specific business questions through SQL queries.
-            </p>
-
-            <div className="flex justify-between items-center ">
-              <a href="https://github.com/louisnguyenn/Retail_Data_Analysis" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)]">View Project</a>
+              <a href="https://github.com/louisnguyenn/Job_Vacancies_Data_Analysis" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)]">View Project</a>
             </div>
           </div>
 
           <div className="flex justify-center space-x-4">
-            <a href="https://github.com/louisnguyenn" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded-full font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)] flex gap-2 items-center">Check out my GitHub <ArrowRight size={16}/></a>
+            <a href="https://github.com/louisnguyenn" target="_blank" className="bg-[#AA8F76] text-white py-3 px-6 rounded-full font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rbga(59, 130, 246, 0.4)] flex gap-2 items-center">Check out my GitHub <ArrowRight size={16} /></a>
           </div>
         </div>
       </div>
