63bf773b1114cb966e27751b1b03f6378aad1ed5
louis nguyen <louis.nguyen550@gmail.com>
2025-07-05 23:40:14 -0400

    formatted profile and paragraph

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,10 +1,11 @@
 import { ScrollReveal } from '../ScrollReveal'
 import StarBorder from '../StarBorder'
+import { Github, Linkedin } from "lucide-react";
 
 export const About = () => {
   return (
     <section id="about" className="min-h-screen flex items-center justify-center py-20 bg-[#02040F]">
-      <div className="max-w-6xl mx-auto px-6">
+      <div className="max-w-5xl mx-auto px-6">
         <ScrollReveal direction="up" distance={50} duration={0.8}>
           <h2 className="text-5xl font-bold mb-4 text-white text-center">About <span className="text-[#AA8F76]">Me</span></h2>
           <hr className="h-1 w-14 m-auto bg-white border-0" />
@@ -22,14 +23,22 @@ export const About = () => {
           color="white"
           speed="5s"
         > */}
-        <div className="flex gap-10 items-start">
+        <div className="flex gap-20 items-start">
           <div className="flex-shrink-0">
             <ScrollReveal direction="up" distance={50} duration={0.8}>
-              <img src="/pfp.jpg" className="w-75 h-75 object-cover rounded-xl border border-white" />
+              <img src="/pfp.jpg" className="w-80 h-80 object-cover rounded-xl border border-[#AA8F76]" />
+              <div className="flex space-x-6 py-7 justify-center">
+                <a href="https://github.com/louisnguyenn" target="_blank" rel="noopener noreferrer" className="text-[#AA8F76] hover:text-white transition-all duration-300 hover:-translate-y-1">
+                  <Github size={32} />
+                </a>
+                <a href="https://www.linkedin.com/in/louisnguyenn/" target="_blank" rel="noopener noreferrer" className="text-[#AA8F76] hover:text-white transition-all duration-300 hover:-translate-y-1">
+                  <Linkedin size={32} />
+                </a>
+              </div>
             </ScrollReveal>
           </div>
 
-          <div className="bg-[#05091e] relative rounded-xl p-8 flex-1 border border-white">
+          <div className="bg-[#05091e] relative rounded-xl p-8 flex-1 border border-white/10 hover:border-[#AA8F76] transition-all duration-300">
             <ScrollReveal direction="up" distance={50} duration={0.8}>
               <p className="text-gray-300 mb-4 leading-relaxed">
                 Hi, my name is Louis. I am currently studying <a href="https://www.uoguelph.ca/programs/engineering-systems-and-computing/" className="underline transition-colors hover:text-white" target="_blank">Engineering Systems and Computing</a> at the <a href="https://www.uoguelph.ca/" className="underline transition-colors hover:text-white" target="_blank">University of Guelph</a>, where I'm passionate about combining both mechanical and software to build systems that solve real world problems.
