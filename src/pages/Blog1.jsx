a17f9ba2be596a7dc5de1fbfaab090dfaf3cc7f4
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-10 18:07:27 -0400

    added placeholder paragraphs with gibberish

--- src/pages/Blog1.jsx
+++ src/pages/Blog1.jsx
@@ -34,6 +34,29 @@ export const Blog1 = () => {
           alt="Linamar Gear plant front entrance"
           className="w-160 h-160"
         ></img>
+
+        <div className="px-4 space-y-6 py-8">
+          <p className="text-gray-300 leading-relaxed text-xl">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+
+          <p className="text-gray-300 leading-relaxed text-xl">
+            Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+
+          <p className="text-gray-300 leading-relaxed text-xl">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+            accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+            quae ab illo inventore veritatis et quasi architecto beatae vitae
+            dicta sunt explicabo.
+          </p>
+        </div>
       </div>
     </div>
   );
