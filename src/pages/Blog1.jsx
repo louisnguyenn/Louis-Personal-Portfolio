e702a02de6b82d8da6790cdc799046488952a1d6
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-10 18:06:54 -0400

    adjusted blog width and styling

--- src/pages/Blog1.jsx
+++ src/pages/Blog1.jsx
@@ -1,26 +1,38 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { ArrowLeft } from "lucide-react";
 
 export const Blog1 = () => {
   const navigate = useNavigate();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const navigateHome = () => {
-    navigate("/Hero");
+    navigate("/");
   };
 
   return (
-    <div className="max-w-4xl mx-auto">
-      <div className="flex flex-col justify-center items-center">
-        <h2 className="text-3xl font-medium pt-15">
+    <div className="max-w-3xl mx-auto">
+      <button
+        onClick={navigateHome}
+        className="flex gap-1 items-center absolute left-4 top-4 px-4 py-2 text-sm font-medium text-gray-300 hover:scale-105 hover:text-white transition-all duration-300 cursor-pointer"
+      >
+        <ArrowLeft size={15} /> Go Back
+      </button>
+      <div className="flex flex-col justify-center items-center pt-20">
+        <h2 className="text-3xl font-medium">
           My time at Linamar Corporation (specifically the Linamar Gear plant)
         </h2>
         <div className="mr-auto py-5">
-          <p className="text-sm">Louis Nguyen</p>
-          <p className="text-xs">AUG 10, 2025</p>
+          <p className="text-sm text-gray-300">Louis Nguyen</p>
+          <p className="text-xs pb-2 text-gray-300">AUG 10, 2025</p>
         </div>
         <img
           src="/Linamar-Gear.jpg"
           alt="Linamar Gear plant front entrance"
-          className="w-160 h-160 py-10"
+          className="w-160 h-160"
         ></img>
       </div>
     </div>
