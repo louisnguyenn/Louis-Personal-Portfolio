4e8b947d88ffacbf25c1c012cd74298bf6b91c70
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-10 17:51:16 -0400

    started the styling of blog, no content yet

--- src/pages/Blog1.jsx
+++ src/pages/Blog1.jsx
@@ -8,8 +8,21 @@ export const Blog1 = () => {
   };
 
   return (
-    <>
-      <h2>Coming soon...</h2>
-    </>
+    <div className="max-w-4xl mx-auto">
+      <div className="flex flex-col justify-center items-center">
+        <h2 className="text-3xl font-medium pt-15">
+          My time at Linamar Corporation (specifically the Linamar Gear plant)
+        </h2>
+        <div className="mr-auto py-5">
+          <p className="text-sm">Louis Nguyen</p>
+          <p className="text-xs">AUG 10, 2025</p>
+        </div>
+        <img
+          src="/Linamar-Gear.jpg"
+          alt="Linamar Gear plant front entrance"
+          className="w-160 h-160 py-10"
+        ></img>
+      </div>
+    </div>
   );
 };
