9d57efec066c271e8f59dc12f775d55d72fe6532
Louis Nguyen <louis.nguyen550@gmail.com>
2025-08-14 21:18:11 -0400

    created blog nav component

--- src/pages/Blog1.jsx
+++ src/pages/Blog1.jsx
@@ -1,27 +1,10 @@
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { ArrowLeft } from "lucide-react";
+import { BlogNavbar } from "../components/BlogNavbar";
 
 export const Blog1 = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-
-  const navigateHome = () => {
-    navigate("/");
-  };
-
   return (
     <div className="max-w-3xl mx-auto">
-      <button
-        onClick={navigateHome}
-        className="flex gap-1 items-center absolute left-4 top-4 px-4 py-2 text-sm font-medium text-gray-300 hover:scale-105 hover:text-white transition-all duration-300 cursor-pointer"
-      >
-        <ArrowLeft size={15} /> Go Back
-      </button>
       <div className="flex flex-col justify-center items-center pt-20">
+        <BlogNavbar />
         <h2 className="text-3xl font-medium">
           My time at Linamar Corporation (specifically the Linamar Gear plant)
         </h2>
