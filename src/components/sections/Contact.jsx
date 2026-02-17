dc65f998b0993c4fe0f0e961e3e8b033143516dd
Louis Nguyen <louis.nguyen550@gmail.com>
2026-02-16 20:08:12 -0500

    add contact logos to contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,3 +1,5 @@
+import { Github, Linkedin, Mail } from 'lucide-react';
+import FadeContent from '../animations/FadeContent';
 import { ScrollReveal } from '../animations/ScrollReveal';
 
 export const Contact = () => {
@@ -5,12 +7,72 @@ export const Contact = () => {
     <section id="contact" className="pt-4 relative overflow-hidden">
       <div className="w-full px-4 sm:px-6 lg:px-8">
         <ScrollReveal>
-          <div className="mb-16">
+          <div className="mb-10">
             <h2 className="text-3xl md:text-4xl font-light mb-4 text-white">
               Let's Connect
             </h2>
           </div>
         </ScrollReveal>
+
+        <div className="mx-auto w-full">
+          <div className="flex gap-10 px-4">
+            <FadeContent
+              blur={true}
+              duration={1000}
+              easing="ease-out"
+              initialOpacity={0}
+              className="md:flex items-center space-x-8"
+            >
+              <div className="group relative">
+                <a href="mailto:louis.nguyen550@gmail.com">
+                  <div className="flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
+                    <Mail size={35} />
+                  </div>
+                </a>
+              </div>
+            </FadeContent>
+
+            <FadeContent
+              blur={true}
+              duration={1000}
+              easing="ease-out"
+              initialOpacity={0}
+              className="md:flex items-center space-x-8"
+            >
+              <div className="group relative">
+                <a
+                  href="https://www.linkedin.com/in/louisnguyenn/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
+                    <Linkedin size={35} />
+                  </div>
+                </a>
+              </div>
+            </FadeContent>
+
+            <FadeContent
+              blur={true}
+              duration={1000}
+              easing="ease-out"
+              initialOpacity={0}
+              className="md:flex items-center space-x-8"
+            >
+              <div className="group relative">
+                <a
+                  href="https://github.com/louisnguyenn"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <div className="flex items-center justify-center group-hover:scale-110 transition-transform duration-300">
+                    <Github size={35} />
+                  </div>
+                </a>
+              </div>
+            </FadeContent>
+          </div>
+        </div>
       </div>
     </section>
   );
