f46ed0126da75fe514c1ea051b17bb6c914df7c3
Louis Nguyen <louis.nguyen550@gmail.com>
2026-06-05 11:36:09 -0400

    update logos

--- src/components/sections/Experience.jsx
+++ src/components/sections/Experience.jsx
@@ -4,11 +4,12 @@ export const Experience = () => {
   const experiences = [
     {
       title: 'Facilities and Maintenance Assistant',
-      company: "RIMOWA",
+      company: 'RIMOWA',
       period: 'June 2026 - August 2026',
-      description: 'Facilities and Maintenance Support for Industrial Assembly and Installation',
+      description:
+        'Facilities and Maintenance Support for Industrial Assembly and Installation',
       link: 'https://www.rimowa.com/ca/en/home',
-      logo: '/logos/rimowa.jpg',
+      logo: '/logos/rimowa_logo.jpg',
     },
     {
       title: 'Production Operator',
@@ -16,7 +17,7 @@ export const Experience = () => {
       period: 'May 2026 - June 2026',
       description: 'Production Operations for Meat Processing and Packaging',
       link: 'https://www.pillers.com/',
-      logo: '/logos/pillers.webp',
+      logo: '/logos/pillers_logo.jpg',
     },
     {
       title: 'CNC Machine Operator',
@@ -25,7 +26,7 @@ export const Experience = () => {
       description:
         'CNC Lathe Operation and Quality Assurance for Precision Automotive Manufacturing',
       link: 'https://www.linamar.com/',
-      logo: '/logos/linamar.webp',
+      logo: '/logos/linamar_logo.jpg',
     },
   ];
 
